6b668085658bf3499493e0dcf0e748af4ade66e0
warren-r-lewis <86686919+warren-r-lewis@users.noreply.github.com>
2021-09-16 19:00:01 -0400

    Add files via upload
    First HW

---
+++ Homework 1/Homework 1/SQLQuery3.sql
@@ -0,0 +1,5 @@
+INSERT INTO Locations(LocationName)Values('Shire');
+INSERT INTO Locations(LocationName)Values('Tatoonine');
+INSERT INTO Locations(LocationName)Values('Ba Sing Se');
+INSERT INTO Locations(LocationName)Values('Whiterun');
+INSERT INTO Locations(LocationName)Values('Wartwood');
---
+++ Homework 1/Homework 1/SQLQuery6.sql
@@ -0,0 +1,5 @@
+INSERT INTO Services(ServiceName) VALUES('Sword Sharpenning');
+INSERT INTO Services(ServiceName) VALUES('Haircut');
+INSERT INTO Services(ServiceName) VALUES('Exorcism');
+INSERT INTO Services(ServiceName) VALUES('Tarot Reading');
+INSERT INTO Services(ServiceName) VALUES('Curse');
